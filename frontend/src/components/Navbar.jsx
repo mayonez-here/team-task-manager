d9cddd0508c4ae407cc972a9e3e45b5eff7859a4
Mohd Shonez <140795032+mayonez-here@users.noreply.github.com>
2026-05-05 14:25:48 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,24 +7,24 @@ export default function Navbar() {
   };
 
   return (
-    <div style={{
-      display: "flex",
-      justifyContent: "space-between",
-      padding: "15px",
-      background: "#111",
-      color: "white"
-    }}>
+    // <div style={{
+    //   display: "flex",
+    //   justifyContent: "space-between",
+    //   padding: "15px",
+    //   background: "#111",
+    //   color: "white"
+    // }}>
       
-      {/* <div style={{ display: "flex", gap: "15px" }}>
-        <Link to="/dashboard" style={{ color: "white" }}>Dashboard</Link>
-        <Link to="/projects" style={{ color: "white" }}>Projects</Link>
-        <Link to="/tasks" style={{ color: "white" }}>Tasks</Link>
+    //   <div style={{ display: "flex", gap: "15px" }}>
+    //     <Link to="/dashboard" style={{ color: "white" }}>Dashboard</Link>
+    //     <Link to="/projects" style={{ color: "white" }}>Projects</Link>
+    //     <Link to="/tasks" style={{ color: "white" }}>Tasks</Link>
         
-      </div> */}
+    //   </div> 
 
-      <button onClick={logout}>
-        Logout
-      </button>
-    </div>
+    //   <button onClick={logout}>
+    //     Logout
+    //   </button>
+    // </div>
   );
 }
